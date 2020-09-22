cb36ea420c3d045bfa86d0d87e55c0f17165f3a0
gonzalocarhuachin <lgonzalocarhuachin@gmail.com>
2020-09-22 01:17:44 -0500

    home y nosotros

---
+++ src/app/entities/ventaja.ts
@@ -0,0 +1,4 @@
+export class Ventaja
+{
+    descripcion: string;
+}
---
+++ src/app/entities/ventajas.ts
@@ -0,0 +1,12 @@
+import { Ventaja } from './ventaja';
+
+export const VENTAJAS: Ventaja[] =
+[
+    {descripcion: "Estar al tanto de los alimentos de tu mascota."},
+    {descripcion: "Recordar y planificar sus próximas citas al veterinario."},
+    {descripcion: "Planificar sus paseos."},
+    {descripcion: "Redactar y editar los datos de tu mascota."},
+    {descripcion: "Crear su perfil!"},
+    {descripcion: "Agregar observaciones de su veterinario."},
+    {descripcion: "Programar sus baños."}
+]
